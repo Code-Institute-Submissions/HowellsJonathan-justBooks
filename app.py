ab68a43520962a200534a18e1847da4c3cab100d
Jon Howells <howells.jonnathan@gmail.com>
2020-11-09 19:07:45 +0000

    Added required imports to app.py file

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
-from flask import Flask
+import os
+from flask import (Flask, flash, render_template,
+                   redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import os
+
+
 app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World ... again for the second time ... testing 1 . 2 . 3"
