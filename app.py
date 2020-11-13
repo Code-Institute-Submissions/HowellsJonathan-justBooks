a60841da5446a471c0448938869194efb30fead3
Jon Howells <howells.jonnathan@gmail.com>
2020-11-13 15:20:30 +0000

    Added functions to login in after registering.

--- app.py
+++ app.py
@@ -24,9 +24,33 @@ def get_books():
     return render_template("all_books.html", books=books)
 
 
-@app.route("/login")
-def login():
+@app.route("/login_page")
+def login_page():
     return render_template("login.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # Check if a username already exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # Check if the hashed password mathes input from user
+            if check_password_hash(existing_user["password"],
+                                   request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                return redirect(url_for("get_books"))
+            else:
+                # Flash is password is wrong
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # Username isn't in the database
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
 
 
 @app.route("/register", methods=["GET", "POST"])
