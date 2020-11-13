3bcb276c31c1503f70e08559b36ebff594159ef7
Jon Howells <howells.jonnathan@gmail.com>
2020-11-13 14:42:26 +0000

    Added register functionality to update to database

--- app.py
+++ app.py
@@ -24,8 +24,30 @@ def get_books():
     return render_template("all_books.html", books=books)
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # Check if a username already exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()}
+        )
+
+        if existing_user:
+            flash("Username Taken")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password")),
+            "bookmarked": [],
+            "my_reviews": [],
+            "my_books": [],
+        }
+        mongo.db.users.insert_one(register)
+
+        # Put the user into session variable to pull on other pages
+        session["user"] = request.form.get("username").lower()
+
     return render_template("register.html")
 
 
