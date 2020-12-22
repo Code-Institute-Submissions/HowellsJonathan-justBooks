d8d10f4742cdcfee8330464c6e3d4cb964ed2ff3
Jon Howells <howells.jonnathan@gmail.com>
2020-12-22 19:44:17 +0000

    Added JS for new swiper plugin

--- static/js/script.js
+++ static/js/script.js
@@ -9,3 +9,27 @@ $(document).ready(function(){
   $('.carousel.carousel-slider').carousel({
     fullWidth: true
   });
+
+import Swiper from "swiper/bundle";
+import "swiper/swiper-bundle.css";
+
+const swiper = new Swiper(".swiper-container", {
+  direction: "horizontal",
+  loop: true,
+
+  // Adds arrows for the user to scroll through if they don't want to swipe
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+
+  // Autoplay the carousel / swiper 
+  autoplay: {
+    delay: 5000,
+  }
+
+  // Disable preloading of all images to speed up load times
+  preloadImages: false,
+
+  lazy: true,
+});
