2bbf20c6b4278c88f8e042cc1ee59255cc3d5b44
Jon Howells <howells.jonnathan@gmail.com>
2020-12-28 14:17:58 +0000

    Styled action cards for desktop view

--- static/js/script.js
+++ static/js/script.js
@@ -10,38 +10,3 @@ $(document).ready(function(){
     fullWidth: true
   });
 
-
-var swiper = new Swiper(".swiper-container", {
-  direction: "horizontal",
-  loop: true,
-
-  // Adds arrows for the user to scroll through if they don't want to swipe
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // Autoplay the carousel / swiper 
-  autoplay: {
-    delay: 5000,
-  },
-
-  // Disable preloading of all images to speed up load times
-  preloadImages: false,
-
-  lazy: true,
-
-  slidesPerView: 1,
-  spaceBetween: 10,
-
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    1024: {
-      slidesPerView: 4,
-      spaceBetween: 50,
-    },
-  },
-});
